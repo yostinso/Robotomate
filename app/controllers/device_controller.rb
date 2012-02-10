e35099181f092b372da5b6c2dbb3c0710b05af9a
E.O. Stinson <yostinso@gmail.com>
2012-02-10 08:39:22 -0800

    Docs and such

---
+++ app/controllers/device_controller.rb
@@ -0,0 +1,14 @@
+class DeviceController < ApplicationController
+  def show
+  end
+
+  def list
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Robotomate::Application.config.secret_token = '80e9c24815fca2a798012ce8372f3d644ed645f2fdfb7e6b8e751ecaa7b019d316edfb53481cf5c096f1cc802dcaf7151b3a2dcd4f708171f3b4ef88044e244d'
