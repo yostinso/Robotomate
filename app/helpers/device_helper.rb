a4cb66e00b20406161b8c66d2a9a46931a92d662
E.O. Stinson <yostinso@gmail.com>
2012-05-08 09:30:40 -0700

    More ridiculous overengineering of the add-device form

--- app/helpers/device_helper.rb
+++ app/helpers/device_helper.rb
@@ -1,2 +1,11 @@
 module DeviceHelper
+  def form_for_device(url, html_options, &wrapper)
+    res = form_tag(url, html_options) do
+      if wrapper
+        wrapper.call(render :partial => "device/device_form")
+      else
+        render :partial => "device/device_form"
+      end
+    end
+  end
 end
